fa2569893efde4f12ff147914960ac28b31d153d
xiejingwen <xiejw@133.cn>
2020-10-28 17:42:20 +0800

    test

--- get.py
+++ get.py
@@ -4,13 +4,16 @@ import pandas as pd
 import os 
 
 
-for gap in [10,20,30,40,50,60,70]:
-    for WAIT in [3,4,5]:
+for gap in [20]:
+    for WAIT in [10]:
         rs1 = 0
         rs2 = 0
         fs1 = []
         fs2 = []
+        time1 = []
+        time2 = []
         for file in os.listdir("D:\\stocks"):
+            z1 = 1
             if file.startswith('sz.30'):
                 continue
             dataset = pd.read_csv("D:\\stocks\\" + file, encoding='gbk')
@@ -20,31 +23,40 @@ for gap in [10,20,30,40,50,60,70]:
             x = []
             buy = 0
             g = 0
+            startInd = 0
             for i in range(gap, num.shape[0]):
-                if num[i, 1] > num[i-gap:i,1].mean():
+                if num[i, 1] > num[i-gap:i,1].mean() and num[i-int(gap/2):i, 4].mean() > num[i-gap:i-int(gap/2), 4].mean():
                     if buy == 0:
                         if g == WAIT :
+                            startInd = i
                             buy = num[i, 1]
                             # print('buy ' , num[i,0])
                         else :g += 1
                     else : g = WAIT
                 if num[i, 1] < num[i-gap:i,1].mean():
                     if buy != 0:
-                        if g <= 0:
+                        if g <= 3:
                             if num[i, 1] > buy:
                                 rs1 += 1
                             rs2 += 1
                             # print('sale ' , num[i,0], (num[i,1]/buy - 1) * 100)
-                            rate = (num[i,1]/buy - 1)
-                            if rate > 0:
-                                fs1.append(rate)
-                            else :
-                                fs2.append(rate)
+                            rate = num[i,1]/buy
+                            z1 *= (rate * 0.997)
+                            qq = i - startInd
+                            if qq > 150:
+                                qq = 150
+                            if rate > 1:
+                                time1.append(qq)
+                            else: time2.append(qq)
                             buy = 0
                         else :g -= 1
                     else: g=0
-        print(gap, WAIT, rs1, rs2, rs1/rs2, np.mean(fs1), np.mean(fs2), (sum(fs1) - sum(fs2))/rs2)
+            fs1.append(z1)
+        print(gap, WAIT, rs1, rs2, rs1/rs2, np.mean(fs1), np.mean(time1), np.mean(time2))
+        # plt.figure()									#打印样本点
+        # plt.scatter(range(len(time1)),time1)						#把x_data和y_data传进来
+        # plt.show()
 
-# plt.figure()									#打印样本点
-# plt.scatter(range(len(fs)),fs)						#把x_data和y_data传进来
-# plt.show()
+        # plt.figure()									#打印样本点
+        # plt.scatter(range(len(time2)),time2)						#把x_data和y_data传进来
+        # plt.show()
--- test.py
+++ test.py
@@ -6,4 +6,52 @@ import time
 import os 
 
 for file in os.listdir("D:\\stocks"):
-    os.rename("D:\\stocks\\" + file, "D:\\stocks\\" + file[0:9] + ".csv")
+    if file.startswith('sz.30'):
+        continue
+    dataset = pd.read_csv("D:\\stocks\\"+file, encoding='gbk')
+    num = dataset.to_numpy()
+    if num.shape[0] == 0 or num[-1,1] > 10:
+        continue
+    x = []
+    w10 = 0
+    w20 = 0
+    w30 = 0
+    w40 = 0
+    a10 = 0
+    gap = 20
+    win = []
+    loss = []
+    for i in range(60, num.shape[0]):
+        if num[i-60:i+gap,1].argmin() == 60 and i + 60 + gap < num.shape[0]:
+            buy = num[i+gap,1]
+            minp = num[i-60:i+gap,1].min()
+            if num[i+10+gap,1] > buy:
+                w10 += 1
+            if num[i+20+gap,1] > buy:
+                w20 += 1
+            if num[i+30+gap,1] > buy:
+                w30 += 1
+            if num[i+40+gap,1] > buy:
+                w40 += 1
+            a10 += 1
+            JACK = 1 + (1 - minp/buy) * 3
+            back = 0
+            c = 0
+            for j in range(i + gap, i+60+gap):
+                if num[j, 1] <= minp:
+                    loss.append(num[j,1]/buy - 1)
+                    c = 2
+                    break
+                elif num[j,1] / buy > JACK:
+                    if back == 1:
+                        win.append(num[j,1]/buy - 1)
+                        c = 1
+                        break
+                    back = 1
+            if c == 0:
+                if num[i+60+gap,1] > buy:
+                     win.append(num[i+40+gap,1]/buy - 1)
+                else : loss.append(num[i+40+gap,1]/buy - 1)
+    if a10 > 0:
+        print(file, w10/a10, w20/a10, w30/a10, w40/a10, a10, len(win)/(len(win) + len(loss)), np.mean(win), np.mean(loss))
+    else : print(file)
