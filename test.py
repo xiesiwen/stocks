f8013160854202f257f9f365c6ec9a37ab8c1cc0
xiejingwen <xiejw@133.cn>
2020-10-28 18:17:09 +0800

    修改testt

--- test.py
+++ test.py
@@ -5,6 +5,56 @@ import pandas as pd
 import time
 import os 
 
+def comK2(i, j, ps):
+        k = (ps[j, 0] - ps[i, 0]) / (j - i)
+        b = ps[i, 1] - k*i
+        return [k,b]
+def loss(ps, res):
+    loss = 0
+    for i in range(ps.shape[0]):
+        r = ps[i, 0] - (res[0] * i + res[1])
+        if r < 0:
+            return 10000000
+        loss += r
+    return loss
+def getBestLineUp(ps):
+    minLoss = -1
+    minLossRes = [-1, 0, 0]
+    for i in range(ps.shape[0]):
+        mink = -1
+        minRes = []
+        for j in range(i + 1, ps.shape[0]):
+            res = comK2(i,j,ps)
+            if mink == -1 or res[0] < mink:
+                mink = res[0]
+                minRes = res
+                minRes.append(j)
+        if len(minRes) > 0:
+            loss1 = loss(ps, minRes)
+            if minLoss == -1 or loss1 < minLoss:
+                minLoss = loss1
+                minLossRes = minRes
+    return minLossRes
+
+def getBestLineDown(ps):
+    minLoss = -1
+    minLossRes = [-1, 0, 0]
+    for i in range(ps.shape[0]):
+        mink = -1
+        minRes = []
+        for j in range(i + 1, ps.shape[0]):
+            res = comK2(i,j,ps)
+            if mink == -1 or res[0] < mink:
+                mink = res[0]
+                minRes = res
+                minRes.append(j)
+        if len(minRes) > 0:
+            loss1 = loss(ps, minRes)
+            if minLoss == -1 or loss1 < minLoss:
+                minLoss = loss1
+                minLossRes = minRes
+    return minLossRes
+    
 for file in os.listdir("D:\\stocks"):
     if file.startswith('sz.30'):
         continue
@@ -12,46 +62,8 @@ for file in os.listdir("D:\\stocks"):
     num = dataset.to_numpy()
     if num.shape[0] == 0 or num[-1,1] > 10:
         continue
-    x = []
-    w10 = 0
-    w20 = 0
-    w30 = 0
-    w40 = 0
-    a10 = 0
-    gap = 20
-    win = []
-    loss = []
-    for i in range(60, num.shape[0]):
-        if num[i-60:i+gap,1].argmin() == 60 and i + 60 + gap < num.shape[0]:
-            buy = num[i+gap,1]
-            minp = num[i-60:i+gap,1].min()
-            if num[i+10+gap,1] > buy:
-                w10 += 1
-            if num[i+20+gap,1] > buy:
-                w20 += 1
-            if num[i+30+gap,1] > buy:
-                w30 += 1
-            if num[i+40+gap,1] > buy:
-                w40 += 1
-            a10 += 1
-            JACK = 1 + (1 - minp/buy) * 3
-            back = 0
-            c = 0
-            for j in range(i + gap, i+60+gap):
-                if num[j, 1] <= minp:
-                    loss.append(num[j,1]/buy - 1)
-                    c = 2
-                    break
-                elif num[j,1] / buy > JACK:
-                    if back == 1:
-                        win.append(num[j,1]/buy - 1)
-                        c = 1
-                        break
-                    back = 1
-            if c == 0:
-                if num[i+60+gap,1] > buy:
-                     win.append(num[i+40+gap,1]/buy - 1)
-                else : loss.append(num[i+40+gap,1]/buy - 1)
-    if a10 > 0:
-        print(file, w10/a10, w20/a10, w30/a10, w40/a10, a10, len(win)/(len(win) + len(loss)), np.mean(win), np.mean(loss))
-    else : print(file)
+    for i in range(num.shape[0]):
+        if num[i,0].startswith('2019-02'):
+            res = getBestLineDown((num[i-60:i,1]/num[i-60,1] -1) * 100)
+            print((num[i-60:i,1]/num[i-60,1] -1) * 100)
+            break            
