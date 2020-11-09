aa460fd91658e02b044fb9cf317408d6e1b82910
xiejingwen <15671559246@163.com>
2020-11-09 21:16:47 +0800

    com

--- compute.py
+++ compute.py
@@ -4,7 +4,17 @@ import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+from sklearn import linear_model
+line = linear_model.LinearRegression()
 
+def getK(dataset):
+    x = np.arange(len(dataset)) + 1
+    # print(dataset,x)
+    z1 = np.polyfit(x.tolist(), dataset.tolist(), 1)
+    plt.scatter(x, dataset)
+    plt.plot(x, z1[0] * x + z1[1])
+    # plt.show()
+    return z1[0]
 GAP1 = 60
 GAP2 = 20
 all = 0
@@ -22,75 +32,24 @@ for file in os.listdir("D:\\stocks"):
         continue
     # print(file)
     c -= 1
-    if c== 0:
+    if c == 0:
         break
     dataset = pd.read_csv("D:\\stocks\\" + file, encoding='gbk')
     num = dataset.to_numpy()
     if num.shape[0] == 0:
         continue
-    for i in range(0,num.shape[0],5):
-        
+    for i in range(120,num.shape[0],5):
         if i + GAP1 + GAP2 < num.shape[0]:
-            all += 1
-            if num[i,0].startswith('2020'):
-                xs_test.append((num[i:i+GAP1,1]/num[i,1] - 1) * 100)
-            else:
-                xs.append((num[i:i+GAP1,1]/num[i,1] - 1) * 100)
-            b = num[i+GAP1,1]
-            s = num[i+GAP1 + GAP2,1]
-            if s / b > 1.2:
-                g20 += 1
-            if s/b > 1.2:
-                g30 += 1
-                if num[i,0].startswith('2020'):
-                    ys_test.append(1)
-                else:
-                    ys.append(1)
-                    yes.append((num[i:i+GAP1,1]/num[i,1] - 1) * 100)
-            else:
-                if num[i,0].startswith('2020'):
-                    ys_test.append(0)
-                else:
-                    ys.append(0)
-
-yes = [val for val in yes for i in range(5)]
-xs.extend(yes)
-ys.extend(np.ones(len(yes)).tolist())
-print(len(xs), len(ys), sum(ys)/len(ys))
-xs = np.array(xs)
-ys = np.array(ys)
-shuffle_ix = np.random.permutation(np.arange(len(xs)))
-xs = xs[shuffle_ix]
-ys = ys[shuffle_ix]
-xs_test = np.array(xs_test)
-ys_test = np.array(ys_test)
-model = tf.keras.Sequential([
-    # tf.keras.layers.Flatten(input_shape=(1,60)),
-    tf.keras.layers.Dense(20,input_shape=(60,),activation="relu"),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(2,activation="softmax")
-])
-# checkpoint_save_path = "g60-30"
-# if os.path.exists(checkpoint_save_path + '.index'):
-#     print('-------------load the model-----------------')
-#     model.load_weights(checkpoint_save_path)
-
-#设置训练参数
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(0.001),
-    loss="sparse_categorical_crossentropy",
-    metrics=['accuracy']
-)
-
-# cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_save_path,
-#                                                  save_weights_only=True,
-#                                                  save_best_only=True,
-#                                
-#                   monitor='val_loss')
-print(xs.shape,ys.shape, g30, g30/len(xs))
-xs = xs.astype('float64')
-xs_test = xs_test.astype('float64')
-#训练并查看训练进度
-history = model.fit(xs,ys,epochs=500,validation_data=(xs_test, ys_test), validation_freq=1)
-model.save('the_save_model.h5')
-model.summary()
+            z = 0
+            for x in range(i+1,i+21):
+                if num[x,1] > num[i,1]:
+                    z += 1
+            if z >= 20 * 0.9:
+                y = 1
+            else : y = 0
+            xs.append(y)
+            k1 = getK(num[i-60:i,1])
+            k2 = getK(num[i-120:i,1])
+            if k1 > k2 and k1 >= 0:
+                ys.append(y)
+print(len(xs), len(ys), sum(xs)/len(xs), sum(ys)/len(ys))
