a3a30f9be74e19c94eddb2f473f0310b902bc5e4
xiejingwen <xiejw@133.cn>
2021-01-15 14:43:19 +0800

    添加计算分钟力度

--- bao.py
+++ bao.py
@@ -12,8 +12,8 @@ def setDir(filepath):
         os.mkdir(filepath)
 def writeStock(stock, key):
     rs = bs.query_history_k_data_plus(stock,
-        "date,close,open,low,high,pctChg,volume,isST",
-        start_date = str(datetime.date.today() + datetime.timedelta(days=-400)), end_date=str(datetime.date.today()),
+        "date,close,open,low,high,volume",
+        start_date = str(datetime.date.today() + datetime.timedelta(days=-30)), end_date=str(datetime.date.today()),
         frequency=key, adjustflag="2")
     #### 打印结果集 ####
     data_list = []
@@ -26,7 +26,23 @@ def writeStock(stock, key):
     #### 结果集输出到csv文件 ####   
     result.to_csv(path + "\\" + stock + ".csv", index=False)
 
-year = "today"
+def writeStock30(stock):
+    rs = bs.query_history_k_data_plus(stock,
+        "date,close,open,low,high,volume,time",
+        start_date = str(datetime.date.today() + datetime.timedelta(days=-30)), end_date=str(datetime.date.today()),
+        frequency="30", adjustflag="2")
+    #### 打印结果集 ####
+    data_list = []
+    while (rs.error_code == '0') & rs.next():
+        # 获取一条记录，将记录合并在一起
+        r = rs.get_row_data()
+        data_list.append(r)
+    result = pd.DataFrame(data_list, columns=rs.fields)
+
+    #### 结果集输出到csv文件 ####   
+    result.to_csv(path + "\\" + stock + ".csv", index=False)
+
+year = "today-m"
 path = "D:\\stocks"+"-"+year
 # setDir(path)
 ### 登陆系统 ####
@@ -36,6 +52,6 @@ dataset = pd.read_csv("D:/stock/all_stock.csv", encoding='gbk')
 for row in dataset.itertuples():
     if row[1].startswith('sh.60') or row[1].startswith('sh.68') or row[1].startswith('sz.00') or row[1].startswith('sz.30'):
         print(row[1])
-        writeStock(row[1], 'd')
+        writeStock30(row[1])
 #### 登出系统 ####
 bs.logout()
--- julei.py
+++ julei.py
@@ -1,74 +1,17 @@
 import numpy as np
 import pandas as pd
-import os,json
-import matplotlib.pyplot as plt
-import mplfinance as mpf
-import shutil
-count = 0
-filepath = "D:/image1"
-RANGE = 120
-DIFF = 40
-def getRect(num, index):
-    diff = num[index - 5:index].mean() - num[index - 10:index].mean()
-    c = 0
-    i = 0
-    start = 0
-    for x in range(index - 1, 20, -1):
-        d = num[x - 5:x].mean() - num[x - 10:x].mean()
-        if d * diff < 0:
-            if start == 0:
-                start = x
-            c += abs(d)
-            i = x
-        elif c > 0:
-            break
-    return [c, i, start - x, index - start]
-def save(file):
-    columns_json_str = '{"date":"Date","open":"Open","close":"Close","high":"High","low":"Low","volume":"Volume"}'
-    columns_dict = json.loads(columns_json_str)
 
-    data=pd.read_csv('D:/stocks/'+file,index_col= 'date',usecols=['date','open','close','high','low','volume'])
-    data.rename(columns=columns_dict, inplace=True)
-    data.index = pd.DatetimeIndex(data.index)
-    mpf.plot(data, type='candle', mav=(5, 10), volume=True, style='charles',savefig=filepath+"/" + file +'.png')
-if not os.path.exists(filepath):
-    os.mkdir(filepath)
-else:
-    shutil.rmtree(filepath)
-    os.mkdir(filepath)
-
-for file in os.listdir("D:/stocks"):
-    if file.startswith('sz.30'):
-        continue
-    dataset = pd.read_csv("D:/stocks/" + file, encoding='gbk').tail(RANGE+DIFF)
-    num = dataset.to_numpy()[:,1]
-    if num.shape[0] < RANGE + DIFF or dataset.to_numpy()[0,-1] != 0:
-        continue
-    size = num.shape[0]
-    ind = num.argmin()
-    
-    if ind < size - DIFF:
-        continue
-    for x in range(ind, size):
-        if num[x - 5:x].mean() > num[x - 10:x].mean():
-            size = x
-            break
-    
-    x = np.arange(0, RANGE)
-    y = np.array(num[size - RANGE:size],dtype='float')
-    f = np.polyfit(x, y, 1)
-    ks = np.poly1d(f)
-    if (ks[1] < 0 and num[size - 5:size].mean() > num[size - 10:size].mean() and num[size - 1] < 10):
-        save1 = False
-        for size in range(size, num.shape[0]):
-            z = getRect(num, size)
-            if z[1] == 0:
-                continue
-            y = getRect(num, z[1])
-            if y[0] > z[0]:
-                save1 = True
-        if save1:
-            print(file, z, y)
-            count += 1
-            save(file)
-print(count)
+file = "sh.601899.csv"
+W = 100000
+dataset = pd.read_csv("D:/stocks-today-m/" + file, encoding='gbk')
+num = dataset.to_numpy()
+dataStart = 202012161000 * W
+dataEnd = 202012301500 * W
+F = 0
+s1 = (np.where(num == dataStart))[0][0]
+s2 = (np.where(num == dataEnd))[0][0]
+F = num[s1,3] * (num[s1+1,2] - num[s1,3]) + num[s2,2] * (num[s2,4] - num[s2,2])
+print(num[s1], num[s2])
+for i in range(s1 + 1, s2):
+    F += num[i,2] * (num[i+1, 2] - num[i,2])
+print(F)
