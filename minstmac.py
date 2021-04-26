e7fb247e091dc9783cf3430fc48306d57ea9e047
xiwjingwen <15671559246@613.com>
2021-04-26 22:40:20 +0800

    mac

---
+++ minstmac.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import os 
+
+LEN = 10
+def jacks(num):
+    tops = [-LEN]
+    bottoms = [-LEN]
+    for i in range(LEN, len(num) - LEN):
+        if num[i,4] >= num[i- LEN: i+LEN, 4].max():
+            if i > tops[-1] + LEN:
+                tops.append(i)
+        if num[i,3] <= num[i- LEN: i+LEN, 3].min():
+            if i > bottoms[-1] + LEN:
+                bottoms.append(i)
+    return tops[1:], bottoms[1:]
+# names = {}
+# data = pd.read_excel("D:/stock/hangye.xlsx", usecols = [0, 1, 4]).to_numpy()
+# for i in range(len(data)):
+#     ss = data[i,2].split('-')
+#     if len(ss) >= 2:
+#         key = ss[2]
+#         ss = data[i,0].lower().split(".")
+#         file = ss[1]+"."+ss[0]+".csv"
+#         names[file] = data[i, 1]
+c = 0
+for file in os.listdir("./stock-today"):
+    if file.startswith('sz.30') :
+        continue
+    dataset = pd.read_csv("./stock-today/" + file, encoding='gbk')
+    num = dataset.to_numpy()
+    if num.shape[0] == 0:
+        continue
+    if num[-1,1] > 20:
+        continue
+    mInd = num[:,1].argmin()
+    tops, btms = jacks(num[mInd:])
+    if len(tops) < 2 or len(btms) < 2 or len(num) - mInd < 40:
+        continue
+    p = True
+    if len(btms) > 1:
+        for i in range(max(1, len(btms)-3), len(btms)):
+            if num[mInd+btms[i], 3] < num[mInd+btms[i-1], 3]*0.95:
+                p = False
+                break
+    for i in range(max(1, len(tops)-3), len(tops)):
+        z = num[mInd+tops[i], 4] / num[mInd+tops[i-1], 4]
+        if z < 0.95 or z > 1.8:
+            p = False
+    #         break
+    # if '002015' in file:
+    #     print(tops, btms, num[np.array(tops)+mInd], num[np.array(btms)+mInd], p)
+    if p and num[mInd+tops[-1], 1] >= num[mInd+tops[-2], 1]*1.08 and num[-1,1] > num[mInd+btms[-1],1]*0.95:
+        print(file)
+        c += 1
+print(c)
